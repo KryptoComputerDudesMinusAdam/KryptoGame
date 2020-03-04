6defcab1665b40a40e2aa72a91188febc2541f18
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-03-03 22:06:34 -0500

    sample code

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,6 +1,28 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try{
+            String clientSentence;
+            String capitalizedSentence;
+            ServerSocket welcomeSocket = new ServerSocket(6789);
+
+            while(true) {
+                Socket connectionSocket = welcomeSocket.accept();
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                clientSentence = inFromClient.readLine();
+                capitalizedSentence = clientSentence.toUpperCase() + '\n';
+                outToClient.writeBytes(capitalizedSentence);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
