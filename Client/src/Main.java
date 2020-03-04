f2414e18d652037219826e32cca28c4d1fdf1a33
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-03-04 00:00:21 -0500

    Reverted back to original client socket

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -1,52 +1,28 @@
 import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         try{
-            Scanner scanInt = new Scanner(System.in);
-            Scanner scanStr = new Scanner(System.in);
+            Scanner scan = new Scanner(System.in);
             System.out.println("Please enter the port number you want to connect to as a client: ");
-            int port = scanInt.nextInt();
-            System.out.println("Please enter the port number you want to connect to as a client: ");
-            String hostname = scanStr.nextLine();
-            if (args.length < 2) return;
+            int port = scan.nextInt();
 
-            try (Socket socket = new Socket(hostname, port)) {
+            String sentence;
+            String modifiedSentence;
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-                OutputStream output = socket.getOutputStream();
-                PrintWriter writer = new PrintWriter(output, true);
+            Socket clientSocket = new Socket("localhost", port);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                Console console = System.console();
-                String text;
-
-                do {
-                    text = console.readLine("Enter text: ");
-
-                    writer.println(text);
-
-                    InputStream input = socket.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-                    String time = reader.readLine();
-
-                    System.out.println(time);
-
-                } while (!text.equals("bye"));
-
-                //socket.close();
-
-            } catch (UnknownHostException ex) {
-
-                System.out.println("Server not found: " + ex.getMessage());
-
-            } catch (IOException ex) {
-
-                System.out.println("I/O error: " + ex.getMessage());
-            }
+            sentence = inFromUser.readLine();
+            outToServer.writeBytes(sentence + '\n');
+            modifiedSentence = inFromServer.readLine();
+            System.out.println(modifiedSentence);
+            clientSocket.close();
         } catch(Exception e){
 
         }
