73578df699c29845a40b4e59316351b74ee88439
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-03-03 22:23:45 -0500

    Simple run works

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,17 +6,37 @@ public class Main {
 
     public static void main(String[] args) {
         try{
-            String clientSentence;
-            String capitalizedSentence;
-            ServerSocket welcomeSocket = new ServerSocket(6789);
+//            String clientSentence;
+//            String capitalizedSentence;
+//            ServerSocket welcomeSocket = new ServerSocket(6789);
+//
+//            while(true) {
+//                Socket connectionSocket = welcomeSocket.accept();
+//                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+//                OutputStream output = connectionSocket.getOutputStream();
+//                DataOutputStream outToClient = new DataOutputStream(output);
+//                clientSentence = inFromClient.readLine();
+//                capitalizedSentence = clientSentence.toUpperCase() + '\n';
+//                outToClient.writeBytes(capitalizedSentence);
+//            }
+            if (args.length < 1) return;
 
-            while(true) {
-                Socket connectionSocket = welcomeSocket.accept();
-                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                clientSentence = inFromClient.readLine();
-                capitalizedSentence = clientSentence.toUpperCase() + '\n';
-                outToClient.writeBytes(capitalizedSentence);
+            int port = Integer.parseInt(args[0]);
+
+            try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+                System.out.println("Server is listening on port " + port);
+
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    System.out.println("New client connected");
+
+                    new ServerThread(socket).start();
+                }
+
+            } catch (IOException ex) {
+                System.out.println("Server exception: " + ex.getMessage());
+                ex.printStackTrace();
             }
         } catch (Exception e){
             e.printStackTrace();
