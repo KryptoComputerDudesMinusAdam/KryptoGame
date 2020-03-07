f3ce7df0e2b5dbe94bd194ebd63363cd7b5b4781
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-03-07 00:09:54 -0500

    Implemented Hash Map

--- Server/src/ServerThread.java
+++ Server/src/ServerThread.java
@@ -1,13 +1,19 @@
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ServerThread extends Thread {
     private Socket socket;
+    private String clientName;
+    private int clientPort;
+    private List<String> messages = new ArrayList<>();
+    private static HashMap<Integer, String> contacts = new HashMap<>();
 
     public ServerThread(Socket socket) {
         this.socket = socket;
+        this.clientPort = socket.getPort();
     }
 
     public void run() {
@@ -16,28 +22,29 @@ public class ServerThread extends Thread {
             OutputStream outputStream = socket.getOutputStream();
             DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
 
-            // send welcome message & rules
-            dataOutputStream.writeUTF(
-                    "WELCOME\n" +
-                            "You have successfully connected to the server\n" +
-                            "For your first message, respond with your name so we can add you into our contacts list\n" +
-                            "Send message 'exit' to terminate connection\n" +
-                            "Below you will find the following contacts list: "+"\n");
-            dataOutputStream.flush();
-
             // for receiving messages
             InputStream inputStream = socket.getInputStream();
             DataInputStream dataInputStream = new DataInputStream(inputStream);
 
-            // read the message from the socket
-            List<String> messages = new ArrayList<>();
+            // send welcome message & rules
+            dataOutputStream.writeUTF(
+                    "WELCOME\n" +
+                            "You have successfully connected to the server\n" +
+                            "For your first message, respond with your name so" +
+                            " we can add you into our contacts list\n" +
+                            "Send message 'exit' to terminate connection\n" +
+                            "Below you will find the following contacts list: \n" +
+                            ServerThread.contacts.toString());
+            dataOutputStream.flush();
+
+            // read the message from the client
             while(socket.isConnected()){
                 String message = dataInputStream.readUTF();
                 messages.add(message);
-
                 if(messages.size() == 1){
                     System.out.println("Received name "+ messages.get(0) + ", adding to hash table");
-                    // TODO: add port number and name to hash table
+                    this.clientName = messages.get(0);
+                    ServerThread.contacts.put(this.socket.getPort(), this.clientName);
                 } else if(message.toLowerCase().equals("exit")){
                     System.out.println(messages.get(0) + " requests to close socket");
                     dataOutputStream.writeUTF(messages.get(0) + ", goodbye.");
