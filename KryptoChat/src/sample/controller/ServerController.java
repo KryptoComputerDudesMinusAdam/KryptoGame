8a7e7d1b35c9680753940a35f58b2895602b5520
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-04-04 17:32:24 -0400

    Added shell server logic

--- KryptoChat/src/sample/controller/ServerController.java
+++ KryptoChat/src/sample/controller/ServerController.java
@@ -1,4 +1,32 @@
 package sample.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class ServerController {
+
+    @FXML
+    Button connectButton, disconnectButton;
+    @FXML
+    TextField portTextField;
+    @FXML
+    ListView listView;
+
+    public void handleConnectButton(ActionEvent event){
+        /*
+            TODO:
+                Add connect to server logic
+
+         */
+    }
+
+    public void handleDisconnectButton(ActionEvent event){
+        /*
+            TODO:
+                Add disconnect to server logic
+         */
+    }
 }
