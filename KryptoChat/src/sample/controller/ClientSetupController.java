79b2606f85f2c1b9465f58307da78696f610540c
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-04-08 16:38:01 -0400

    Server can now connect with multiple clients and broadcast updated contacts list to each client

--- KryptoChat/src/sample/controller/ClientSetupController.java
+++ KryptoChat/src/sample/controller/ClientSetupController.java
@@ -14,7 +14,9 @@ import sample.model.MessageList;
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class ClientSetupController {
 
@@ -35,35 +37,11 @@ public class ClientSetupController {
                 clientNameTextField.getText() != null){
             String host = hostnameTextField.getText();
             int port = Integer.parseInt(serverTextField.getText());
-            new Thread(() -> {
-                try(Socket socket = new Socket(host, port)) {
-                    // for sending messages
-                    OutputStream outputStream = socket.getOutputStream();
-                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-
-                    // for receiving messages
-                    InputStream inputStream = socket.getInputStream();
-                    ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-
-                    // add your name into the server's contacts list
-                    objectOutputStream.writeObject(new Message(clientNameTextField.getText()));
-
-                    // wait for contacts list to render
-                    initializeListView();
-                    new Thread(() ->{ // continuously update list view with contacts list
-                        try {
-                            MessageList ms = (MessageList) objectInputStream.readObject();
-                            if(ms.typeOfMessage != null && ms.typeOfMessage.equals(Message.contacts)){
-                                displayContacts(ms);
-                            }
-                        } catch (IOException | ClassNotFoundException e) {
-                            e.printStackTrace();
-                        }
-                    }).start();
-                }catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-            }).start();
+            ClientServerThread clientServerThread = new ClientServerThread(this);
+            clientServerThread.port = port;
+            clientServerThread.host = host;
+            clientServerThread.clientName = clientNameTextField.getText();
+            clientServerThread.start();
             }
     }
 
@@ -98,10 +76,78 @@ public class ClientSetupController {
 
     public void displayContacts(MessageList ms){
         Platform.runLater(() -> {
+            initializeListView();
             contacts.clear();
             contacts.addAll(ms.messages);
             observableList.setAll(contacts);
             contactsListView.setItems(observableList);
         });
+    }
+}
+
+class ClientServerThread extends Thread {
+    String host;
+    int port;
+    String clientName;
+    ClientSetupController clientSetupController;
+    Queue<MessageList> contactsQueue = new LinkedList<>();
+    Queue<Message> messageQueue = new LinkedList<>();
+
+    public ClientServerThread(ClientSetupController clientSetupController){
+        this.clientSetupController = clientSetupController;
+    }
+
+    public void run(){
+        try {
+            // Create a socket to connect to the server
+            Socket socket = new Socket(host, port);
+
+            // for sending messages
+            OutputStream outputStream = socket.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+            // for receiving messages
+            InputStream inputStream = socket.getInputStream();
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+            // add your name into the server's contacts list
+            objectOutputStream.writeObject(new Message(clientName));
+
+            // listen in for incoming messages
+            new Thread(()->{
+                while(true){
+                    boolean isMessageList = false;
+                    try {
+                        MessageList ms = (MessageList) objectInputStream.readObject();
+                        clientSetupController.displayContacts(ms);
+                        isMessageList = true;
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    if(!isMessageList){
+                        try {
+                            Message m = (Message) objectInputStream.readObject();
+                            messageQueue.add(m);
+                        } catch (IOException | ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class ContactsList extends Thread {
+
+    public ContactsList(){
+
+    }
+
+    public void run(){
+
     }
 }
