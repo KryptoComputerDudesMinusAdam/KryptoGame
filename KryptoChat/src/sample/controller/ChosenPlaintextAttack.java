1d63f4d0808e4ecb7ce7d142f2915a98b6a4500e
Manohar Chitoda <manohar.chitoda@rutgers.edu>
2020-04-09 14:09:22 -0400

    Update on disconnection

--- KryptoChat/src/sample/controller/ChosenPlaintextAttack.java
+++ KryptoChat/src/sample/controller/ChosenPlaintextAttack.java
@@ -1,6 +1,32 @@
 package sample.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class ChosenPlaintextAttack extends AttackerSetupController
 {
+    @FXML
+    Button disconnect;
 
+    public void goBack(ActionEvent event)
+    {
+        try{
+            // display user interface
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("../view/AttackerSetup.fxml"));
+            Parent root = loader.load();
+            AttackerSetupController UI = loader.getController();
+            UI.init();
+            Controller.newWindow(root);
+
+            Stage stage = (Stage) disconnect.getScene().getWindow();
+            stage.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- KryptoChat/src/sample/controller/KnownPlaintextAttack.java
+++ KryptoChat/src/sample/controller/KnownPlaintextAttack.java
@@ -1,6 +1,40 @@
 package sample.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
 public class KnownPlaintextAttack extends AttackerSetupController
 {
+    @FXML
+    Button disconnect, query_server, run_analysis, copy_message;
 
+    @FXML
+    ListView<String> ciphertext, plaintext, response;
+
+    public void init()
+    {
+
+    }
+    public void goBack(ActionEvent event)
+    {
+        try{
+            // display user interface
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("../view/AttackerSetup.fxml"));
+            Parent root = loader.load();
+            AttackerSetupController UI = loader.getController();
+            UI.init();
+            Controller.newWindow(root);
+
+            Stage stage = (Stage) disconnect.getScene().getWindow();
+            stage.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
